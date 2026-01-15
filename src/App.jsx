30a97c83dd8626c61fc9d2dfb3875618d5acad5d
Sachindra <sachindra.pandey@stigasoft.com>
2026-01-15 16:49:10 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login.jsx'
-import Register from './pages/Register.jsx'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <h1></h1>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
