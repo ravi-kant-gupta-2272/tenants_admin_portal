b2a3008f0566915bc43e82c134b839b634ee674f
Ravi Kant Gupta <kantgupta1995@gmail.com>
2026-02-03 10:46:29 +0530

    changes in login screen ui.

--- src/components/TextInputField.jsx
+++ src/components/TextInputField.jsx
@@ -1,4 +1,7 @@
-import { TextField } from "@mui/material";
+import { TextField, IconButton, InputAdornment } from "@mui/material";
+import { useState } from "react";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function TextInputField({
   fullWidth = true,
@@ -9,29 +12,48 @@ function TextInputField({
   value,
   onChange,
   required,
+  hidePassword = false
 }) {
+  const [isObscure, setobscure] = useState(hidePassword);
+
+  const handleClickShowPassword = () => setobscure((show) => !show);
+
   return (
     <TextField
       fullWidth={fullWidth}
       label={label}
       name={name}
-      type={type}
+      type={hidePassword ? (isObscure ? type : "text" ): type}
       margin={margin}
       value={value}
       onChange={onChange}
       required={required}
+      InputProps={{
+        endAdornment: hidePassword && (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label={isObscure ? "Show password" : "Hide password"}
+              onClick={handleClickShowPassword}
+              onMouseDown={(e) => e.preventDefault()}
+              edge="end"
+              sx={{color:"#ffffff"}}
+            >
+              {isObscure ? <VisibilityOff color="#ffffff" /> : <Visibility color="#ffffff"/>}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
       sx={{
-        label: { color: "#ffffff" },
-        color: "#ffffff", // this applies to text and label in most cases
-        '& .MuiOutlinedInput-root': {
-          '& fieldset': { borderColor: "#ffffff" },
-          '&:hover fieldset': { borderColor: "#ffffff" },
-          '&.Mui-focused fieldset': { borderColor: "#ffffff" },
+        "& .MuiInputLabel-root": { color: "#fff" },
+        "& .MuiInputBase-input": { color: "#fff" },
+        "& .MuiOutlinedInput-root": {
+          "& fieldset": { borderColor: "#fff" },
+          "&:hover fieldset": { borderColor: "#fff" },
+          "&.Mui-focused fieldset": { borderColor: "#fff" },
         },
-        '& .MuiInputLabel-root': { color: "#ffffff" },
-        '& .MuiInputBase-input': { color: "#ffffff" },
       }}
     />
+
   );
 }
 
